70dc2dc271ace88f1e1253180523f9c0025635dd
geocvt <camivtass@gmail.com>
2025-04-25 10:25:58 -0700

    updates

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 const basemapPortalID = "12f2664ae36f452f95f42cdfecb39586"; //Default basemap portal id 
 const routesPortalID = "a8fb797c78c44d3da37a0c728e66d604";
+const HubsandFacilitiesPortalID = "e9ef4b395acb4d2c92e6f7bf7c0a1c63";
+const barriersPortalID = "7029c8ab0c0a4761ad59cefd7192bfc6";
